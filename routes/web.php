7d95e8d47fc9164322f3b103c6c353a9a507c22a
AlbertWebs <albertmuhatia@gmail.com>
2025-10-24 19:44:47 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/about', [HomeController::class, 'about'])->name('about-us');
+Route::get('/about-us', [HomeController::class, 'about'])->name('about-us-alt');
 Route::get('/eva-naputuni-nyoike', [HomeController::class, 'director'])->name('the-director');
 Route::get('/our-story', [HomeController::class, 'story'])->name('our-story');
 Route::get('/our-history', [HomeController::class, 'history'])->name('our-history');
