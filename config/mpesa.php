5e509850d7005d724ae5e5655620096c5f91cc3e
AlbertWebs <albertmuhatia@gmail.com>
2025-11-12 21:38:16 +0300

    c2b now.

--- config/mpesa.php
+++ config/mpesa.php
@@ -3,7 +3,7 @@
 return [
     'consumer_key' => env('MPESA_CONSUMER_KEY', ''),
     'consumer_secret' => env('MPESA_CONSUMER_SECRET', ''),
-    'short_code' => env('MPESA_SHORT_CODE', '174379'), // Sandbox default paybill
+    'short_code' => env('MPESA_SHORT_CODE', '3574755'), // Sandbox default paybill
     'passkey' => env('MPESA_PASSKEY', ''),
     'base_url' => env('MPESA_BASE_URL', 'https://api.safaricom.co.ke'),
     'callback_url' => env('MPESA_STK_CALLBACK', env('APP_URL') . '/mpesa/stk/callback'),
